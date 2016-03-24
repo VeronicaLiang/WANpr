0918be91b7407a99684626a4fbd02f54c46fa845
Xiaoyu <veronica.xiaoyu@gmail.com>
2016-03-24 12:31:05 -0400

    register self to the file

--- src/sLSRP.java
+++ src/sLSRP.java
@@ -5,6 +5,7 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.ServerSocket.*;
 import java.io.*;
+import java.nio.file.NoSuchFileException;
 
 public class sLSRP {
     public static String ip;
@@ -21,6 +22,7 @@ public class sLSRP {
         try {
             file = args[0];
             conf.configuration(file);
+            conf.register();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -30,7 +32,6 @@ public class sLSRP {
 //        System.out.println("My ip: " + ip);
 //        String hostname = inetAddr.getHostName();
 //        System.out.println("My host name: "+hostname);
-
 
         ServerThread ser = new ServerThread();
         new Thread (ser).start();
@@ -42,6 +43,12 @@ public class sLSRP {
         LSAThread lsa = new LSAThread();
         new Thread(lsa).start();
 
-        //todo delete host_list file
+
+        File register_file = new File("host_list");
+        try {
+            register_file.delete();
+        }  catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
